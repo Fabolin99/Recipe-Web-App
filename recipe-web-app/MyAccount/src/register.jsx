5c7602b4381f9d3eb0fb625b374a20fa80f6aaa9
dlee0616 <98242430+dlee0616@users.noreply.github.com>
2023-05-08 10:11:57 -0700

    css file
    css file

--- recipe-web-app/MyAccount/src/register.jsx
+++ recipe-web-app/MyAccount/src/register.jsx
@@ -9,7 +9,8 @@ export const Register = () => {
         console.log(email);
     }
     return (
-        <>
+        <div className="form-container">
+            <h2>Register</h2>
             <form onSubmit = {handleSubmit}>
                 <label htmlFor="name">Full Name</label>
                 <input value = {name} name = "name" id = "name" placeholder = "full name" onChange = {(e) => setName(e.target.value)}></input>
@@ -19,7 +20,7 @@ export const Register = () => {
                 <input value = {password} onChange = {(e) => setPass(e.target.value)} type = "password" placeholder="*******" id = "password" name = "password"></input>
                 <button type = "submit">Register</button>
             </form>
-            <button onClick = {() => props.OnFormSwitch('login')}>Already registered? Login</button>
-        </>
+            <button className = "link-button" onClick = {() => props.OnFormSwitch('login')}>Already registered? Login</button>
+        </div>
     )
 }
