086a65259e8a356d7dc16879f9633608718610d2
Porter Hatch <porterhatch1@gmail.com>
2023-05-22 16:39:16 -0600

    made it pretty, added checkmarks, and a css file

--- recipe-web-app/src/IngredientList.js
+++ recipe-web-app/src/IngredientList.js
@@ -1,56 +1,50 @@
-// Porter Hatch 
-// May 10th 2023
+import React, { useState } from "react";
+import "./IngredientList.css";
 
-import React, { Component } from 'react';
-export default class IngredientList extends Component {}
-
-class IngredientList extends React.Component {
-    
-    ingredients = [
-        [1, "item", "eggs"],
-        [2, "lbs", "chicken"],
-        [3, "lbs", "hamburger"],
-        [12, "item", "tortillas"],
-        [3, "item", "eggs"], 
-        [12, "item", "blueberry muffins"], 
-        [3,"cups", "flour"],
-        [2, "tbsp", "baking soda"],
-        [2, "cups", "flour"], 
-        [4, "cups", "milk"], 
-        [1, "item", "pickles"],
-        [1,"item", "celery"]
-    ];
-    constructor(props) {
-        super(props);
-        this.state = {
-            ingredients: IngredientList.CompileIngredients(this.ingredients)
-        };
-    }
-    static CompileIngredients(ingredients) {
-        
-        const combinedItems = {};
-    
-        for (let i = 0; i <= ingredients.length; i++) {
-            const [quantity, unit, name] = ingredients[i];
-            const key = name.toLowerCase(); // Use lowercase name as key
-                
-            // If this item has already been added, increment the quantity
-            if (combinedItems[key]) {
-            combinedItems[key][0] += quantity;
-            }
-            // Otherwise, add a new entry for this item
-            else {
-            combinedItems[key] = [quantity, unit, name];
-            }
-        }
-        return Object.values(combinedItems);
-    }
-    ingredients = IngredientList.CompileIngredients(this.ingredients)
-    
-    
-
-    render() {
-        return <h2>{JSON.stringify(this.state.ingredients)}</h2>;
-    }
+function IngredientList() {
+  const checkBox = (index) => {
+    const updatedIngredients = [...ingredients];
+    updatedIngredients[index][3] = !updatedIngredients[index][3];
+    setIngredients(updatedIngredients);
+  };
+  const [ingredients, setIngredients] = useState([
+    [1, "item", "eggs"],
+    [2, "lbs", "chicken"],
+    [3, "lbs", "hamburger"],
+    [12, "item", "tortillas"],
+    [3, "item", "eggs"],
+    [12, "item", "blueberry muffins"],
+    [3, "cups", "flour"],
+    [2, "tbsp", "baking soda"],
+    [2, "cups", "flour"],
+    [4, "cups", "milk"],
+    [1, "item", "pickles"],
+    [1, "item", "celery"],
+  ]);
+  const compileIngredients = () => {
+    return (
+      <ul className="ingredientList">
+        {ingredients.map((ingredient, index) => (
+          <li id="ingredientItem" key={index}>
+            <label>
+              <input
+                type="checkbox"
+                checked={ingredient[3]}
+                onChange={() => checkBox(index)}
+              />
+              {`${ingredient[0]} ${ingredient[1]} of ${ingredient[2]}`}
+            </label>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+  return (
+    <div>
+      <h1 className="title">Ingredients</h1>
+      {compileIngredients()}
+    </div>
+  );
 }
 
+export default IngredientList;
