fd2609750d994d1f602083f780fc44c1edf79817
Porter Hatch <porterhatch1@gmail.com>
2023-06-06 14:05:46 -0600

    saved something

--- recipe-web-app/src/components/NavigationBar.js
+++ recipe-web-app/src/components/NavigationBar.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const NavigationBar = () => {
-  const [activeItem, setActiveItem] = useState('');
+  const [activeItem, setActiveItem] = useState("");
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleItemClick = (itemName) => {
     setActiveItem(itemName);
-    navigate(itemName)
+    navigate(itemName);
     // Perform any additional logic or actions for item selection
   };
 
@@ -16,32 +16,38 @@ const NavigationBar = () => {
     <div className="navigation-bar">
       <div className="logo">Logo</div>
       <div
-        className={`navigation-item ${activeItem === 'Home' ? 'active' : ''}`}
-        onClick={() => handleItemClick('/')}
+        className={`navigation-item ${activeItem === "Home" ? "active" : ""}`}
+        onClick={() => handleItemClick("/")}
       >
         Discover
       </div>
       <div
-        className={`navigation-item ${activeItem === 'About' ? 'active' : ''}`}
-        onClick={() => handleItemClick('create')}
+        className={`navigation-item ${activeItem === "About" ? "active" : ""}`}
+        onClick={() => handleItemClick("create")}
       >
         Create
       </div>
       <div
-        className={`navigation-item ${activeItem === 'Services' ? 'active' : ''}`}
-        onClick={() => handleItemClick('cookbooks')}
+        className={`navigation-item ${
+          activeItem === "Services" ? "active" : ""
+        }`}
+        onClick={() => handleItemClick("cookbooks")}
       >
         Cookbooks
       </div>
       <div
-        className={`navigation-item ${activeItem === 'Contact' ? 'active' : ''}`}
-        onClick={() => handleItemClick('plan')}
+        className={`navigation-item ${
+          activeItem === "Contact" ? "active" : ""
+        }`}
+        onClick={() => handleItemClick("plan")}
       >
         Plan
       </div>
       <div
-        className={`navigation-item ${activeItem === 'Contact' ? 'active' : ''}`}
-        onClick={() => handleItemClick('myaccount')}
+        className={`navigation-item ${
+          activeItem === "Contact" ? "active" : ""
+        }`}
+        onClick={() => handleItemClick("myaccount")}
       >
         My Account
       </div>
