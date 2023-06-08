e2853bc6e0c4c4221c0f4a7842076a7309982abc
dlee0616 <98242430+dlee0616@users.noreply.github.com>
2023-06-08 13:22:24 -0700

    mm

--- recipe-web-app/src/App.js
+++ recipe-web-app/src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import LoginPage from './MyAccount/LoginPage';
+import { useState } from 'react';
+import Register from './MyAccount/Register';
 function App() {
+
+  const [formSwitch, setOnFormSwitch] = useState(true)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={()=>setOnFormSwitch(!formSwitch)}></button>
+        {
+          formSwitch?(<Register />):(<LoginPage />)
+        }
+        
+        
       </header>
     </div>
   );
