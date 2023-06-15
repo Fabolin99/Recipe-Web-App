0fd6aceb992e0ab6d961edf04f5bb4a971fd3ba5
Changmok Lee <mokcandy97@gmail.com>
2023-06-15 13:58:25 -0600

    nav bar fixed

--- recipe-web-app/src/components/NavigationBar.js
+++ recipe-web-app/src/components/NavigationBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import './Navigation.css';
 
 const NavigationBar = () => {
     const [activeItem, setActiveItem] = useState('');
