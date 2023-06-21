3188b522ac498485be792e3592262c0fca8be9ea
Changmok Lee <mokcandy97@gmail.com>
2023-06-21 13:13:58 -0600

    add logo

--- recipe-web-app/src/components/NavigationBar.js
+++ recipe-web-app/src/components/NavigationBar.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './Navigation.css';
+import logo from './Recipe-logos_white.png';
 
 const NavigationBar = () => {
     const [activeItem, setActiveItem] = useState('');
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
     const handleItemClick = (itemName) => {
         setActiveItem(itemName);
@@ -13,47 +14,52 @@ const NavigationBar = () => {
         // Perform any additional logic or actions for item selection
     };
 
-  return (
-    <div className="navigation-bar">
-      <div className="logo">Logo</div>
-      <div
-        className={`navigation-item ${activeItem === "Home" ? "active" : ""}`}
-        onClick={() => handleItemClick("/")}
-      >
-        Discover
-      </div>
-      <div
-        className={`navigation-item ${activeItem === "About" ? "active" : ""}`}
-        onClick={() => handleItemClick("create")}
-      >
-        Create
-      </div>
-      <div
-        className={`navigation-item ${
-          activeItem === "Services" ? "active" : ""
-        }`}
-        onClick={() => handleItemClick("cookbooks")}
-      >
-        Cookbooks
-      </div>
-      <div
-        className={`navigation-item ${
-          activeItem === "Contact" ? "active" : ""
-        }`}
-        onClick={() => handleItemClick("plan")}
-      >
-        Plan
-      </div>
-      <div
-        className={`navigation-item ${
-          activeItem === "Contact" ? "active" : ""
-        }`}
-        onClick={() => handleItemClick("myaccount")}
-      >
-        My Account
-      </div>
-    </div>
-  );
+    return (
+        <div className="navigation-bar">
+            <div
+                className={`logo ${activeItem === "Home" ? "active" : ""}`}
+                onClick={() => handleItemClick("/")}
+            >
+                <img src={logo} alt="Logo" className="logo-img"/>
+            </div>
+            <div
+                className={`navigation-item ${activeItem === "Home" ? "active" : ""}`}
+                onClick={() => handleItemClick("/")}
+            >
+                Discover
+            </div>
+            <div
+                className={`navigation-item ${activeItem === "About" ? "active" : ""}`}
+                onClick={() => handleItemClick("create")}
+            >
+                Create
+            </div>
+            <div
+                className={`navigation-item ${
+                    activeItem === "Services" ? "active" : ""
+                }`}
+                onClick={() => handleItemClick("cookbooks")}
+            >
+                Cookbooks
+            </div>
+            <div
+                className={`navigation-item ${
+                    activeItem === "Contact" ? "active" : ""
+                }`}
+                onClick={() => handleItemClick("plan")}
+            >
+                Plan
+            </div>
+            <div
+                className={`navigation-item ${
+                    activeItem === "Contact" ? "active" : ""
+                }`}
+                onClick={() => handleItemClick("myaccount")}
+            >
+                My Account
+            </div>
+        </div>
+    );
 };
 
 export default NavigationBar;
